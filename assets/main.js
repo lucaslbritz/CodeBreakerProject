527d55d589c9f3180ecb281c20f61ea326992597
Lucas Britz <lucas.britz@hotmail.com>
2018-01-11 17:32:21 +0100

    Create setHiddenFields function.

--- assets/main.js
+++ assets/main.js
@@ -1,9 +1,22 @@
 let answer = document.getElementById('answer');
 let attempt = document.getElementById('attempt');
 
+const MULTIPLIER = 10000;
+const ANSWER_LENGTH = 4;
+
 function guess() {
     let input = document.getElementById('user-guess');
     //add functionality to guess function here
+function setHiddenFields() {
+  answer.value = Math.floor(Math.random() * MULTIPLIER);
+
+  while (answer.value.length < ANSWER_LENGTH) {
+    answer.value = "0" + answer.value;
+  }
+
+  attempt.value = "0";
+}
+
 }
 
 //implement new functions here

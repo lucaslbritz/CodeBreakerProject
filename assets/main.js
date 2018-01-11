dfacc9e62e645216d33c01265c6a3ac7e29dc1ac
Lucas Britz <lucas.britz@hotmail.com>
2018-01-11 17:39:00 +0100

    Create getResults function.

--- assets/main.js
+++ assets/main.js
@@ -41,6 +41,34 @@ function validateInput(input) {
   return true;
 }
 
+function getResults(input) {
+  let result = '';
+  let corrects = 0;
+
+  for (var i = 0; i < input.length; i++) {
+    if (answer.value.charAt(i) === input.charAt(i)) {
+      result += `<span class="glyphicon glyphicon-ok">${input.charAt(i)}</span>`;
+      corrects++;
+      continue;
+    }
+
+    for (var j = 0; j < answer.value.length; j++) {
+      if (answer.value.charAt(j) === input.charAt(i)) {
+        result += `<span class="glyphicon glyphicon-transfer">${input.charAt(i)}</span>`;
+        break;
+      } else if (j === answer.value.length - 1) {
+        result += `<span class="glyphicon glyphicon-remove">${input.charAt(i)}</span>`;
+        break;
+      }
+    }
+  }
+
+  document.getElementById('results').innerHTML = `<div class="row">
+    <span class="col-md-6">${result}</span><div class="col-md-6"></div></div>`;
+
+  return (corrects === ANSWER_LENGTH) ? true : false;
+}
+
 }
 
 //implement new functions here
